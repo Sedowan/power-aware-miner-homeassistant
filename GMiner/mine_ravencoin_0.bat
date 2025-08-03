3b89a9ca4da9f6748b61cc3d45c8304930cf589e
Max <30201448+Sedowan@users.noreply.github.com>
2025-08-03 21:38:16 +0200

    Update mine_ravencoin_0.bat

--- GMiner/mine_ravencoin_0.bat
+++ GMiner/mine_ravencoin_0.bat
@@ -1,4 +1,4 @@
 title GMiner_GPU0
 cd C:\Mining\GMiner
-miner.exe --algo kawpow --server rvn.2miners.com:6060 --user RTnRGQERM4cdaezcuz3v3Rb2CKWvCZ4H9e.Mining-Rig-1 --devices 0
+miner.exe --algo kawpow --server rvn.2miners.com:6060 --user [YOUR_WALLET_ADDRESS].[YOUR_WORKER_ID] --devices 0
 pause
